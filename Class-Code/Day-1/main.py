c2ec0c205c86cafa4316793b03c38394b7129299
Kamran-it009 <kamran.it009@gmail.com>
2026-07-03 16:09:52 +0500

    updated Day-1

---
+++ Class-Code/Day-1/main.py
@@ -0,0 +1,24 @@
+print('My name is Muhammad Kamran')
+print("My name is Muhammad Kamran")
+print(209)
+print('Muhammad', 'Kamran', 'Khan')
+print(203, 304, 'Khan')
+print(203 + 45)
+# print('203' + 45) Error
+print('Muhammad', 'Kamran', 'Khan', sep='---')
+
+print('Hello', end='\n')
+print('How', end= ' ')
+print('are you')
+
+print("This is Doctor's advice")
+print('He said, "I am happy"')
+print('This is Doctor\'s advice')
+
+print('''Gemini is a generative artificial intelligence chatbot and virtual assistant developed by Google.
+It is powered by the family of large language models of the same name, after previously being based 
+on LaMDA and PaLM 2.''')
+
+print('Gemini is a generative artificial intelligence chatbot and virtual assistant developed by Google. '
+      'It is powered by the family of large language models of the same name, after previously being based '
+      'on LaMDA and PaLM 2.')
